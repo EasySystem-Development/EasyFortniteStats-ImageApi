0cce4afcb0752d3e727e796f3024bb7ef54c4479
Lucas Hardt <Luc1412.lh@gmail.com>
2022-12-23 16:41:51 +0100

    wip locker

---
+++ Controllers/LockerImageController.cs
@@ -0,0 +1,150 @@
+﻿using EasyFortniteStats_ImageApi.Models;
+
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Memory;
+
+using SkiaSharp;
+
+// ReSharper disable InconsistentNaming
+namespace EasyFortniteStats_ImageApi.Controllers;
+
+[ApiController]
+[Route("locker")]
+public class AccountImageController : ControllerBase
+{
+    private readonly IMemoryCache _cache;
+    private readonly IHttpClientFactory _clientFactory;
+    private readonly NamedLock _namedLock;
+    private readonly SharedAssets _assets;
+
+    public AccountImageController(IMemoryCache cache, IHttpClientFactory clientFactory, NamedLock namedLock, SharedAssets assets)
+    {
+        _cache = cache;
+        _clientFactory = clientFactory;
+        _namedLock = namedLock;
+        _assets = assets;
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Post(Locker locker)
+    {
+        await _namedLock.WaitAsync($"locker_{locker.RequestId}");
+        var itemsCards = await GenerateItemCards(locker);
+
+        using var lockerBitmap = await GenerateImage(locker, itemsCards);
+        _namedLock.Release($"locker_{locker.RequestId}");
+        
+        var data = lockerBitmap.Encode(SKEncodedImageFormat.Png, 100);
+        return File(data.AsStream(true), "image/png");
+    }
+
+    private async Task<SKBitmap> GenerateImage(Locker locker, IReadOnlyDictionary<string, SKBitmap> itemCards)
+    {
+        // Calculate rows and columns based on locker.Items count
+        // Columns and Rows should be equal
+        // min value is 5
+        var columns = new[] {(int) Math.Ceiling(Math.Sqrt(locker.Items.Length)), 5}.Max();
+        var rows = locker.Items.Length / columns + (locker.Items.Length % columns == 0 ? 0 : 1);
+
+        var nameFontSize = 64;
+        
+        var imageInfo = new SKImageInfo(
+            50 + 256 * columns + 25 * (columns - 1) + 50, 
+            50 + nameFontSize + 25 + rows * 313 + (rows - 1) * 25 + 50);
+        var bitmap = new SKBitmap(imageInfo);
+        using var canvas = new SKCanvas(bitmap);
+
+        var column = 0; var row = 0;
+        foreach (var item in locker.Items)
+        {
+            canvas.DrawBitmap(
+                itemCards[item.Id], 
+                50 + 256 * column + 25 * (column - 1), 
+                50 + nameFontSize + 25 + row * 313 + (row - 1) * 25);
+            column++;
+            if (column != columns) continue;
+            column = 0;
+            row++;
+        }
+
+        return bitmap;
+    }
+    
+    private async Task<Dictionary<string, SKBitmap>> GenerateItemCards(Locker locker)
+    {
+        Dictionary<string, SKBitmap> itemCards = new();
+
+        var options = new ParallelOptions
+        {
+            MaxDegreeOfParallelism = Environment.ProcessorCount / 2
+        };
+        await Parallel.ForEachAsync(locker.Items, options, async (item, token) =>
+        {
+            var itemCard = _cache.Get<SKBitmap?>($"locker_card_image_{item.Id}_{locker.Locale}");
+            if (itemCard == null)
+            {
+                var itemImage = _cache.Get<byte[]?>($"locker_image_{item.Id}");
+                if (itemImage == null)
+                {
+                    var client = _clientFactory.CreateClient();
+                    itemImage = await client.GetByteArrayAsync(item.ImageUrl, token);
+                    //_cache.Set($"locker_image_{item.Id}", itemImage, TimeSpan.FromDays(1));
+                }
+                item.Image = SKBitmap.Decode(itemImage);
+                //_cache.Set($"locker_card_image_{item.Id}_{locker.Locale}", item.Image);
+
+                itemCard = await GenerateItemCard(item);
+            }
+            itemCards.Add(item.Id, itemCard);
+        });
+        return itemCards;
+    }
+
+    private async Task<SKBitmap> GenerateItemCard(LockerItem lockerItem)
+    {
+        var imageInfo = new SKImageInfo( 256, 313);
+        var bitmap = new SKBitmap(imageInfo);
+        using var canvas = new SKCanvas(bitmap);
+        
+        var rarityBackground = await _assets.GetBitmap($"Assets/Images/Locker/RarityBackgrounds/{lockerItem.Rarity}.png");
+        canvas.DrawBitmap(rarityBackground, new SKPoint());
+        
+        canvas.DrawBitmap(lockerItem.Image, new SKPoint());
+
+        var typeIcon = lockerItem.SourceType != SourceType.Other ? await _assets.GetBitmap($"Assets/Images/Locker/Source/{lockerItem.SourceType}.png") : null;
+        using var overlayImage = ImageUtils.GenerateItemCardOverlay(imageInfo.Width, typeIcon);
+        canvas.DrawBitmap(overlayImage, new SKPoint(0, imageInfo.Height - overlayImage.Height));
+        
+        using var rarityStripe = ImageUtils.GenerateRarityStripe(imageInfo.Width, SKColor.Parse(lockerItem.RarityColor));
+        canvas.DrawBitmap(rarityStripe, new SKPoint(0, imageInfo.Height - overlayImage.Height - rarityStripe.Height + 5));
+        // TODO: Fix Transparency issues
+        
+        var fortniteFont = await _assets.GetFont(@"Assets/Fonts/Fortnite.ttf"); // don't dispose
+        
+        using var namePaint = new SKPaint();
+        namePaint.IsAntialias = true;
+        namePaint.TextSize = 18;
+        namePaint.Color = SKColors.White;
+        namePaint.Typeface = fortniteFont;
+        namePaint.TextAlign = SKTextAlign.Center;
+        
+        var entryNameTextBounds = new SKRect();
+        namePaint.MeasureText(lockerItem.Name, ref entryNameTextBounds);
+        
+        canvas.DrawText(lockerItem.Name, (float)bitmap.Width / 2, bitmap.Height - 59 + entryNameTextBounds.Height, namePaint);
+        
+        using var descriptionPaint = new SKPaint();
+        descriptionPaint.IsAntialias = true;
+        descriptionPaint.TextSize = 15;
+        descriptionPaint.Color = SKColor.Parse(lockerItem.RarityColor);
+        descriptionPaint.Typeface = fortniteFont;
+        descriptionPaint.TextAlign = SKTextAlign.Center;
+        
+        descriptionPaint.MeasureText(lockerItem.Description, ref entryNameTextBounds);
+        
+        canvas.DrawText(lockerItem.Description, (float)bitmap.Width / 2, bitmap.Height - 42 + entryNameTextBounds.Height, descriptionPaint);
+
+        return bitmap;
+    }
+    
+}
---
+++ ImageUtils.cs
@@ -0,0 +1,95 @@
+﻿using SkiaSharp;
+
+namespace EasyFortniteStats_ImageApi;
+
+public class ImageUtils
+{
+    public static SKBitmap RotateBitmap(SKBitmap bitmap, float angle)
+    {
+        var radians = MathF.PI * angle / 180;
+        var sine = MathF.Abs(MathF.Sin(radians));
+        var cosine = MathF.Abs(MathF.Cos(radians));
+        int originalWidth = bitmap.Width, originalHeight = bitmap.Height;
+        var rotatedWidth = (int) (cosine * originalWidth + sine * originalHeight);
+        var rotatedHeight = (int) (cosine * originalHeight + sine * originalWidth);
+
+        var rotatedBitmap = new SKBitmap(rotatedWidth, rotatedHeight);
+        using var rotatedCanvas = new SKCanvas(rotatedBitmap);
+        rotatedCanvas.Clear();
+        rotatedCanvas.Translate(rotatedWidth / 2f, rotatedHeight / 2f);
+        rotatedCanvas.RotateDegrees(-angle);
+        rotatedCanvas.Translate(-originalWidth / 2f, -originalHeight / 2f);
+        rotatedCanvas.DrawBitmap(bitmap, SKPoint.Empty);
+
+        return rotatedBitmap;
+    }
+    
+    public static SKBitmap GenerateRarityStripe(int width, SKColor rarityColor)
+    {
+        var imageInfo = new SKImageInfo(width, 14);
+        var bitmap = new SKBitmap(imageInfo);
+        using var canvas = new SKCanvas(bitmap);
+
+        using var paint = new SKPaint();
+        {
+            paint.IsAntialias = true;
+            paint.Color = rarityColor;
+            paint.Style = SKPaintStyle.Fill;
+
+            using var path = new SKPath();
+            path.MoveTo(0, imageInfo.Height - 5);
+            path.LineTo(imageInfo.Width, 0);
+            path.LineTo(imageInfo.Width, imageInfo.Height - 6);
+            path.LineTo(0, imageInfo.Height);
+            path.Close();
+
+            canvas.DrawPath(path, paint);
+        }
+
+        return bitmap;
+    }
+    
+    public static SKBitmap GenerateItemCardOverlay(int width, SKBitmap? icon = null)
+    {
+        var imageInfo = new SKImageInfo(width, 65);
+        var bitmap = new SKBitmap(imageInfo);
+        using var canvas = new SKCanvas(bitmap);
+
+        using (var paint = new SKPaint())
+        {
+            paint.IsAntialias = true;
+            paint.Color = new SKColor(14, 14, 14);
+            paint.Style = SKPaintStyle.Fill;
+
+            canvas.DrawRect(0, 0, imageInfo.Width, imageInfo.Height, paint);
+        }
+
+        if (icon is not null)
+        {
+            using var rotatedVbucksBitmap = RotateBitmap(icon, -20);
+            using var resizedVBucksBitmap = rotatedVbucksBitmap.Resize(new SKImageInfo(47, 47), SKFilterQuality.Medium);
+
+            canvas.DrawBitmap(resizedVBucksBitmap, new SKPoint(imageInfo.Width - 45, imageInfo.Height - 35));
+        }
+
+        using (var paint = new SKPaint())
+        {
+            paint.IsAntialias = true;
+            paint.Color = new SKColor(30, 30, 30);
+            paint.Style = SKPaintStyle.Fill;
+
+            using var path = new SKPath();
+            path.MoveTo(0, imageInfo.Height - 29);
+            path.LineTo(imageInfo.Width, imageInfo.Height - 29);
+            path.LineTo(imageInfo.Width, imageInfo.Height - 25);
+            path.LineTo(0, imageInfo.Height - 24);
+            path.Close();
+
+            canvas.DrawPath(path, paint);
+
+            canvas.DrawRect(0, 0, imageInfo.Width, imageInfo.Height - 29, paint);
+        }
+
+        return bitmap;
+    }
+}
