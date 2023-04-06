7d8111e6f52934b615c59eb8c9dc2315bb9c0f94
Lucas Hardt <Luc1412.lh@gmail.com>
2023-04-07 01:16:07 +0200

    Revert new quality settings

--- Controllers/LockerImageController.cs
+++ Controllers/LockerImageController.cs
@@ -38,11 +38,19 @@ public class AccountImageController : ControllerBase
         
         var qualityMapping = new Dictionary<int, int>
         {
-            {170, 100},
-            {270, 95},
-            {350, 90},
-            {440, 85},
-            {500, 80}
+            {130, 100},
+            {200, 95},
+            {275, 90},
+            {325, 85},
+            {360, 80},
+            {400, 75},
+            {450, 70},
+            {500, 65},
+            // {170, 100}, Values for 25mb limit
+            // {270, 95},
+            // {350, 90},
+            // {440, 85},
+            // {500, 80}
         };
         
         // Determine the quality of the image based on quality mapping and locker.Items.Length
